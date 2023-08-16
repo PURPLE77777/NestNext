bf23912a486cadb9aeb4218ef12e23040cf06a43
56859 <C:\Users\User\AppData\Roaming\The Bat!>
2023-08-16 15:32:47 +0300

    start set

---
+++ back/src/auth/auth.service.ts
@@ -0,0 +1,107 @@
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { AuthDto } from './dto/auth.dto'
+import { faker } from '@faker-js/faker'
+import { hash, verify } from 'argon2'
+import { JwtService } from '@nestjs/jwt'
+import { User } from '@prisma/client'
+
+@Injectable()
+export class AuthService {
+	constructor(
+		private prisma: PrismaService,
+		private jwt: JwtService
+	) {}
+
+	async getNewToken(refreshToken: string) {
+		const result = await this.jwt.verifyAsync(refreshToken)
+		console.log(result)
+		if (result) throw new UnauthorizedException('Invalid refresh token')
+
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id: result.id
+			}
+		})
+
+		const tokens = await this.issueToken(user.id)
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens
+		}
+	}
+
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto)
+	}
+
+	private async validateUser(dto: AuthDto) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email
+			}
+		})
+
+		if (!user) throw new NotFoundException('The user not found')
+
+		const isValid = await verify(user.password, dto.password)
+
+		if (!isValid) throw new UnauthorizedException('InvalidPassword')
+
+		return user
+	}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email
+			}
+		})
+
+		if (oldUser) throw new BadRequestException('The user already exists')
+
+		const user = await this.prisma.user.create({
+			data: {
+				email: dto.email,
+				name: faker.person.firstName(),
+				avatarPath: faker.image.avatar(),
+				phone: faker.phone.number('+375 (##) ###-##-##'),
+				password: await hash(dto.password)
+			}
+		})
+
+		const tokens = await this.issueToken(user.id)
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens
+		}
+	}
+
+	private async issueToken(userId: number) {
+		const data = { id: userId }
+
+		const accessToken = this.jwt.sign(data, {
+			expiresIn: '1h'
+		})
+
+		const refreshToket = this.jwt.sign(data, {
+			expiresIn: '7d'
+		})
+
+		return { accessToken, refreshToket }
+	}
+
+	private returnUserFields(user: User) {
+		return {
+			id: user.id,
+			email: user.email
+		}
+	}
+}
